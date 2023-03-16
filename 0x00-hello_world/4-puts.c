2040924e991bfde209673cbbe9efe2f95653657c
seroking <baridmohamedamine004@gmail.com>
2023-03-16 08:55:30 -0700

    C puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 
 int main() {
 	    
-	         char text[]="Programming is like building a multilingual puzzle";
+	         char text[]="\"Programming is like building a multilingual puzzle";
 		 puts(text);
 	    
 	             return 0;
