0ed69be784cee08e66309e6c2e569de627a8799f
seroking <baridmohamedamine004@gmail.com>
2023-03-16 15:27:52 -0700

    last task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,8 +5,15 @@
  * Description: 'the program's description
  * Return: Always 0 (Success)
  */
-int main(void) {
-  char* message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-  write(2, message, 59);
-  return 1;
+int main(void)
+{
+	char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, message, 59);
+	return (1);
 }
+
+
+
+
+
