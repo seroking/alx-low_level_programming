ecf98b02c18086382df55b33ed830fd3e560974b
seroking <baridmohamedamine004@gmail.com>
2023-03-16 09:05:09 -0700

    print with puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
-int main() {
-	    
+int main(void) {
+	    \\ print with pu a string
 	         char text[]="\"Programming is like building a multilingual puzzle";
 		 puts(text);
 	    
