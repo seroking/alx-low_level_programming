7e0cf1f8b502c65d041c137681b6a2ee55b8b10f
seroking <baridmohamedamine004@gmail.com>
2023-03-16 09:32:25 -0700

    print with puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,6 +4,7 @@
 /*Return: Always 0 (Success) */
 int main(void)
 {
+	/*main function succes*/
 	/* This is a block comment */
 	char str[] = "\"Programming is like building a multilingual puzzle";
 	/*Print the string to the console using puts */
