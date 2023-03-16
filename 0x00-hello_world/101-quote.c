8646913fd8d1435d731c70294ded326cc2f1d3d9
seroking <baridmohamedamine004@gmail.com>
2023-03-16 15:05:38 -0700

    last task

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ * Description: 'the program's description
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char*txt[]="and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	write(2, message, 43);
+}
