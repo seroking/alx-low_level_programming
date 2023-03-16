54c52ace6b6070f40f085c449d5f8bd953e1441e
seroking <baridmohamedamine004@gmail.com>
2023-03-16 09:20:12 -0700

    print with puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,20 +1,11 @@
 #include <stdio.h>
 
-/**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
+/* IDK */
 int main(void)
 {
-		/* This is a block comment */
-		char str[] = "\"Programming is like building a multilingual puzzle";
-			
-			/*
-			 * 	 * This is another block comment
-			 * 	 	 * Print the string to the console using puts
-			 * 	 	 	 */
-			puts(str);
-				
-				return (0);
+	/* This is a block comment */
+	char str[] = "\"Programming is like building a multilingual puzzle";
+	/*Print the string to the console using puts */
+	puts(str);
+	return (0);
 }
