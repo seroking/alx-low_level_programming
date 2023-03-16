15ae1405ec90e89786aae253b381712885b3f5fe
seroking <baridmohamedamine004@gmail.com>
2023-03-16 15:16:47 -0700

    last task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	char* txt="and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-	write(2, txt, 59);
+	char* txt="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, txt, 61);
 	return 1;
 }
