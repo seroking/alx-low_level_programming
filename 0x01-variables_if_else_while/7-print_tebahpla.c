f8c8c8340aa97d2872d0eacd81ce023e94b68e42
seroking <baridmohamedamine004@gmail.com>
2023-03-17 17:42:40 -0700

    final push

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - A program that prints the lowercase alphabet in reverse,
+ * followed by a new line.
+ * Return: 0 (Success)
+*/
+int main(void)
+{
+	char a = 'z';
+
+	while (a >= 'a')
+	{
+		putchar(a);
+		a = a - 1;
+	}
+	putchar('\n');
+	return (0);
