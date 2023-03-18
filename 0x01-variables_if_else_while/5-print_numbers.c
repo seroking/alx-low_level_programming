bb5b3efb4dd2dff36c1bc74d9c6f232e4419448e
seroking <baridmohamedamine004@gmail.com>
2023-03-17 17:01:25 -0700

    print digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+# include <stdio.h>
+/**
+ * main - rogram that prints the alphabet in lowercase, and then in uppercase,
+ * followed by a new line
+ * Return: 0 (Success)
+*/
+int main(void)
+{
+	int a = 0;
+
+
+	while (a < 10)
+	{
+		printf("%d" , a);
+		a++;
+	}
+	return (0);
+}
