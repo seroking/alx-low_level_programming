9675a5cb21126e29f2a898893dad1c61d06d2633
seroking <baridmohamedamine004@gmail.com>
2023-03-21 14:22:52 -0700

    let's go

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,12 +1,17 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
- * main - check the code.
+ * _islower - check the code.
  *
  * Return: Always 0.
+ *@c:character and parametre of the function
  */
 
 int _islower(int c)
 {
-	printf("%d", islower(c));
+	if (c >= 97 && c <= 122)
+	return (1);
+
+	else
+	return (0);
 }
