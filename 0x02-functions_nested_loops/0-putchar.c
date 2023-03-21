0c89422cc46b0fba4dc9f5e8d21ee18c743641e8
seroking <baridmohamedamine004@gmail.com>
2023-03-21 07:15:45 -0700

    printf

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,10 +3,8 @@
 /**
  * main - Entry point
  * Description: 'the program's description'
- * @parameter: describe the parameter
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	printf("_putchar\n");
