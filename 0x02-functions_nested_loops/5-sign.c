2a745cdbf5853f49f057f977fc46853b8981a9b6
seroking <baridmohamedamine004@gmail.com>
2023-03-21 15:12:00 -0700

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,14 @@
+include "main.h"
+
+int print_sign(int n)
+{
+	if (n > 0)
+	printf("+")
+	return (1);
+	else if (n == 0);
+	printf("0")
+	return (0)
+	else if (n < 0)
+	return (-1)
+	printf("-")
+}
