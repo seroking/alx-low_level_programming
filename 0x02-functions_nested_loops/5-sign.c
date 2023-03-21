6dcc93978f5b08bd70741b91fca76c73bd0ba561
seroking <baridmohamedamine004@gmail.com>
2023-03-21 15:56:37 -0700

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,12 +3,18 @@
 int print_sign(int n)
 {
 	if (n > 0)
+	{
 	return (1);
-	putchar('+');
+	_putchar('+');
+	}
 	if (n == 0)
+	{
 	return (0);
-	putchar('0');
+	_putchar('0');
+	}
 	if (n < 0)
+	{
 	return (-1);
-	putchar('-');
+	_putchar('-');
+	}
 }
