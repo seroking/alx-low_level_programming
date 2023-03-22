c8cbf52644cfaa9a61e3c6af29f59348433b3721
seroking <baridmohamedamine004@gmail.com>
2023-03-21 18:25:45 -0700

    absolute

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,9 +1,18 @@
 #include "main.h"
-
-int print_last_digit(int r)
+/**
+* print_last_digit - a fucntion that print last degit of a number
+* @ld: number last degit result
+* Return: value of the last degit
+*/
+int print_last_digit(int ld)
 {
-	int n = (r % 10);
+	int pld;
 
-	printf("%d",n);
-	return (0);
+	pld = (ld % 10);
+	if (pld < 0)
+	{
+		pld = (-1 * pld);
+	}
+	_putchar(pld + '0');
+	return (pld);
 }
