428a8f55a72284e4b6766a023b10806140b233df
seroking <baridmohamedamine004@gmail.com>
2023-03-21 17:56:52 -0700

    absolute

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
-* _abs - a fucntion that computes the absolute value of an integer
-* @ab: integer input
+* _abs - a function that computes the absolute value of an integer
+* @r: integer input
 * Return: absolute value of ab
 */
 int _abs(int r)
