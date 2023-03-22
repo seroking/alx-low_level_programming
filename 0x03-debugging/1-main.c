75f261175c17299622c5c825d771439bcdbfc082
seroking <baridmohamedamine004@gmail.com>
2023-03-22 09:41:56 -0700

    C

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -17,12 +17,12 @@ int main(void)
 	{
 		putchar(i);
 	}
-/**
-while (i < 10)
+	/**
+	while (i < 10)
         {
-                putchar(i);
+		putchar(i);
         }
-*/
+	*/
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
