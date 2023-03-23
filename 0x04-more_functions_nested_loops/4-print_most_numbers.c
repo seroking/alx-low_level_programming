d33d9db414a44b869c22c154d4e7bdee47c19f3c
seroking <baridmohamedamine004@gmail.com>
2023-03-23 08:55:34 -0700

    print most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_most_numbers - function
+ *
+ * Return: Always 0.
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i != 2 && i != 4)
+		{
+			_putchar(i + '0');
+		}
+	}
+	_putchar('\n');
+}
