af50f8a879c9f3e90509108406afcb3c073599d7
seroking <baridmohamedamine004@gmail.com>
2023-03-23 10:20:41 -0700

    print more numbers

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_line(int n)
+{
+	x ;
+
+	for (x = 0; x <= n; x++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n')
+}
