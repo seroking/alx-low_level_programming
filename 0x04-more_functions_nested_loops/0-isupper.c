0e138f7624f4662007d08b3272ade4099d00824e
seroking <baridmohamedamine004@gmail.com>
2023-03-23 07:21:26 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _isupper - function return 1 or 0
+ * @c : variable verified
+ * Return: Always 0.
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	return (0);
+}
