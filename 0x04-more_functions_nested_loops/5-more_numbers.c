ad920ee3bf03da7cf3409b32d9c2dd5717caeaf1
seroking <baridmohamedamine004@gmail.com>
2023-03-23 09:16:43 -0700

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers - check the code
+ *
+ * Return: Always 0.
+ */
+
+void more_numbers(void)
+{
+	int n;
+	int x = 0 ;
+
+	while (x < 10)
+	{
+		for (n = 0; n < 14; n++)
+		{
+			_putchar(n + '0');
+
+		}
+		_putchar('\n')
+		x++
+	}
+}
