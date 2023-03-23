d4896513d662f4dcee1dcd80cfeb8933768f4941
seroking <baridmohamedamine004@gmail.com>
2023-03-23 11:20:00 -0700

    print line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -4,13 +4,16 @@
  * @n : variable
  * Return: Always 0.
  */
-void print_line(int n) {
-	if (n <= 0) {
+void print_line(int n)
+{
+	if (n <= 0)
+	{
 		putchar('\n');
 		return;
 	}
 
-	for (int i = 0; i < n; i++) {
+	for (int i = 0; i < n; i++)
+	{
 		putchar('_');
 	}
 
