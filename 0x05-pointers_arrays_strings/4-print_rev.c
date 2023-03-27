31572f9517f1ac9584b12dc2b340abd75875ffb1
seroking <baridmohamedamine004@gmail.com>
2023-03-27 10:27:16 -0700

    reverse str

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+void print_rev(char *s)
+{
+	printf("%s",strrev(a));
+}
