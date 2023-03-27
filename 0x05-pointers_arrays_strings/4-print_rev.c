774ba2c3e53e07d6db0083093a41d586f603936e
seroking <baridmohamedamine004@gmail.com>
2023-03-27 12:41:36 -0700

    reverse str

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,23 +1,25 @@
-#include <stdio.h>
 #include "main.h"
-#include <string.h>
 
 /**
- * print_rev - check the code
- * @s : parametre string
- * Return: Always 0.
+ * print_rev - a function that print reverse of a string
+ * @s: pointer variable
+ * Return: void return nothing
  */
 
 void print_rev(char *s)
 {
-	int len = strlen(s);
-	int i;
+	int i, lenght;
 
-	for (i = 0; i < len / 2; i++)
+	i = 0;
+	while (s[i] != '\0')
 	{
-		char x = s[i];
-		s[i] = s[len - i - 1];
-		s[len - i - 1] = x;
+		i++;
 	}
-	printf("%s",s);
+
+	lenght = i;
+	for (i = lenght - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
 }
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,6 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
-
+#include <stdio.h>
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
