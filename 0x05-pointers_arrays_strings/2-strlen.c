5e5ae055d7f06a27081480ba75f259994690b07b
seroking <baridmohamedamine004@gmail.com>
2023-03-27 07:23:01 -0700

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strlen - check the code
+ * @*s : parametre
+ * Return: Always 0.
+ */
+
+int _strlen(char *s)
+{
+	strlen(*s);
+	return (0);
+}
