0bd31bb1c7618b09d7c8c676ff27d27ecf7eba9b
seroking <baridmohamedamine004@gmail.com>
2023-03-30 10:59:09 -0700

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,5 +1,14 @@
 #include "main.h"
 
+
+/**
+ * _strcmp - check the code
+ * @s1 : pointer1
+ * @s2 : pointer2
+ * Return: Always 0.
+ */
+
+
 int _strcmp(char *s1, char *s2)
 {
 	while (*s1 && (*s1 == *s2))
@@ -7,5 +16,5 @@ int _strcmp(char *s1, char *s2)
 		s1++;
 		s2++;
 	}
-	return *(const unsigned char*)s1 - *(const unsigned char*)s2;
+	return (*(const unsigned char *)s1 - *(const unsigned char *)s2);
 }
