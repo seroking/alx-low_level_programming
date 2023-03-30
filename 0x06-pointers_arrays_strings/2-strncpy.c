a696c4a717f2e512e7ab5f3e372849758f1a0901
seroking <baridmohamedamine004@gmail.com>
2023-03-30 09:17:08 -0700

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -16,11 +16,12 @@ char *_strncpy(char *dest, char *src, int n)
 	{
 		dest[i] = src[i];
 		i++;
-		while (i < n);
-		{
-			i++;
-			dest[i] == '\0';
-		}
 	}
+
+	for ( ; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+
 	return (dest);
 }
