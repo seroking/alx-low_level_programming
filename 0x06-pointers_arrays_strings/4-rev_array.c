d723b68f666a85c5e48e81d5491363cd19f7ee66
seroking <baridmohamedamine004@gmail.com>
2023-03-30 13:41:46 -0700

    reverse array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+
+/**
+ * main - check the code
+ * @a: an array of integers
+ * @n: the number of elements to swap
+ *
+ * Return: nothing.
+ */
+
+void reverse_array(int *a, int n)
+{
+	for (n = 0; n > 0; n--)
+	{
+		return (a[n]);
+	}
+}
