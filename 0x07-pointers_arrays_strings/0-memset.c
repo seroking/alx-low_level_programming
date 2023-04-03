5811121244fac3f174f3d258a851a5a27ae7707e
seroking <baridmohamedamine004@gmail.com>
2023-04-03 10:38:05 -0700

    0.memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * *_memset - memset func
@@ -12,7 +13,7 @@ char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
-	for (i = 0; i < b; i++)
+	for (i = 0; i < n; i++)
 	{
 		s[i] = b;
 	}
