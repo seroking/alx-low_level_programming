7e4c3fd769a8dce6134514634ab9293119519520
seroking <baridmohamedamine004@gmail.com>
2023-04-03 11:08:17 -0700

    memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _memcpy - prints buffer in hexa
+ * @src : source
+ * @dest : destination
+ * @n : numb of bytes from memory area
+ * RETURN : dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dst[i] = src[i];
+	}
+}
