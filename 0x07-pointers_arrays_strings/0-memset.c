7dc99fe541969c927bf84a4cfc100ba44f36fb7a
seroking <baridmohamedamine004@gmail.com>
2023-04-03 10:08:12 -0700

    _memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,8 +10,11 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	int i =size;
-	for(i = 0;i<size;i++){
-		*(char*)(a+i) = *(char *)(n+(i%4));
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		*(s + i) = b;
 	}
+	return (s);
 }
