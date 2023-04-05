ea039cd08bb8647528084ac37a6cfefec8086ba7
seroking <baridmohamedamine004@gmail.com>
2023-04-05 13:27:04 -0700

    strlen recursion

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -12,12 +12,11 @@ int _strlen_recursion(char *s)
 
 	if (*s == '\0')
 	{
-		return (x);
+		return (0);
 	}
 	if (*s != '\0')
 	{
-		x++;
-		_strlen_recursion(s);
+		return ( 1 + _strlen_recursion(s + 1));
 	}
 	return (0);
 }
