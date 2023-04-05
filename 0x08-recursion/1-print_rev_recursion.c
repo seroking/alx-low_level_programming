fe0c5cd99c94739259fda06b8e4fec064700984d
seroking <baridmohamedamine004@gmail.com>
2023-04-05 10:58:49 -0700

    reverse str recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - check the code
+ * @s : pointer to a string
+ * Return: Always 0.
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	_putchar(*(s - 1));
+	if (*(s - 1) != '\0')
+	{
+		_print_rev_recursion(s);
+	}
+	else
+	{
+		return;
+	}
+}
