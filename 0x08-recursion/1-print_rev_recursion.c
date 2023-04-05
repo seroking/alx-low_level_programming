960f7dac0e6f9cc6d71ae8b5888b7c0092614fd4
seroking <baridmohamedamine004@gmail.com>
2023-04-05 11:33:02 -0700

    reverse str recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,20 +1,17 @@
 #include "main.h"
+#include <unistd.h>
+
 /**
- * _print_rev_recursion - check the code
- * @s : pointer to a string
- * Return: Always 0.
+ *_print_rev_recursion - prints
+ * @s: an array of pointers
+ *Return: nothing void
  */
+
 void _print_rev_recursion(char *s)
 {
-
-	if (*s != '\0')
-	{
-		s++;
-		_print_rev_recursion(s);
-	}
-	else if (*s == '\0');
-	{
-		_putchar(*(s-1));
-		_print_rev_recursion(s);
-	}
+	if (*s == '\0')
+		return;
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
 }
+
