0dd43880d2868b20287e2bd53275b51d625f1654
seroking <baridmohamedamine004@gmail.com>
2023-04-05 10:11:42 -0700

    first recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,17 +7,13 @@
 
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
-	{
-		_putchar('\n');
-	}
 	_putchar(*s);
 
 	if (*(s + 1) != '\0')
 	{
 		_puts_recursion(s + 1);
 	}
-	else if (*s == '\0')
+	else
 	{
 		_putchar('\n');
 	}
