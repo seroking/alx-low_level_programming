17210d391ab7340ac9ce95b429be1c0fc16da7d6
seroking <baridmohamedamine004@gmail.com>
2023-04-05 14:15:13 -0700

    strlen recursion

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * factorial - check the code
+ * @n : numb
+ * Return: Always 0.
+ */
+
+int factorial(int n)
+{
+	if (n == 0)
+	{
+		return (-1);
+	}
+	if (n > 0)
+	{
+		return (n * factorial(n-1));
+	}
+}
