a14365bd9e91fc32e481c270eab41ea828768562
seroking <baridmohamedamine004@gmail.com>
2023-04-05 09:48:16 -0700

    first recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _puts_recursion - puts copy
+ * @s : pointer string argument
+ * Return: Always 0.
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	_putchar(*s);
+	if (*(s+1) != '\0')
+	{
+		_puts_recursion(s+1);
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
