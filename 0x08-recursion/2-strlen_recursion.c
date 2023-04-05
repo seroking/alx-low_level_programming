472e3857102f74025f4994bf6da133599998bca0
seroking <baridmohamedamine004@gmail.com>
2023-04-05 13:07:45 -0700

    strlen recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * int _strlen_recursion - check the code
+ * @s : pointer
+ * Return: Always 0.
+ */
+
+int strlen_recursion(char *s)
+{
+	int x;
+
+	if (*s == '\0')
+	{
+		return(x);
+	}
+	if (*s != '\0')
+	{
+		x++;
+		srlen_recursion(s);
+	}
+}
