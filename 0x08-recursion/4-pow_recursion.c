45c5465fd56b117144bf40909b21f88f1bf7dfa4
seroking <baridmohamedamine004@gmail.com>
2023-04-05 18:08:04 -0700

    pow

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -13,9 +13,9 @@ int _pow_recursion(int x, int y)
 	{
 		return (-1);
 	}
-	if (y > 0)
+	if (y == 0)
 	{
-		return (x * _pow_recursion(x, y - 1));
+		return (1);
 	}
-return;
+	return (x * _pow_recursion(x, y - 1));
 }
