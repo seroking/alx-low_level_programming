623760597aa94fde86471a594f22dda129b5c22f
seroking <baridmohamedamine004@gmail.com>
2023-04-07 08:27:46 -0700

    arg count

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * @argc : count
+ * @argv : argv
+ * Return: Always 0 (Success)
+ */
+
+int main (int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	(void)argv;
+	return 0;
+}
