9a80d1b8cc98b6d6da783d884116ca5b9c07ed45
seroking <baridmohamedamine004@gmail.com>
2023-04-09 10:36:32 -0700

    add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -19,17 +19,18 @@ int main(int argc, char *argv[])
 	if (argc == 1)
 	{
 		printf("0\n");
-		return 0;
+		return (0);
 	}
 	for (i = 1; i < argc; i++)
 	{
 		int j;
+
 		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			if (!isdigit(argv[i][j])) 
+			if (!isdigit(argv[i][j]))
 			{
 				printf("Error\n");
-				return 1;
+				return (1);
 			}
 		}
 		num = atoi(argv[i]);
@@ -39,5 +40,5 @@ int main(int argc, char *argv[])
 		}
 	}
 	printf("%d\n", sum);
-	return 0;
+	return (0);
 }
