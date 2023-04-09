d86055856c7c797bf343621df1e8a34405eade2e
seroking <baridmohamedamine004@gmail.com>
2023-04-09 10:31:01 -0700

    add

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * @argc : count
+ * @argv : argv
+ * Return: Always return 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+	int i;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return 0;
+	}
+	for (i = 1; i < argc; i++)
+	{
+		int j;
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j])) 
+			{
+				printf("Error\n");
+				return 1;
+			}
+		}
+		int num = atoi(argv[i]);
+		if (num > 0)
+		{
+			sum += num;
+		}
+	}
+	printf("%d\n", sum);
+	return 0;
+}
