0181e13cbe4128ff45df4f81c71a924ee116df6f
seroking <baridmohamedamine004@gmail.com>
2023-04-11 08:43:51 -0700

    array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * create_array - prints buffer in hexa
+ * @size : the address of memory to print
+ * @c : the size of the memory to print
+ *
+ * Return: char.
+ */
+
+char *create_array(unsigned int size, char c)
+{
+
+	char* arr = (char*) malloc(size * sizeof(char));
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	if (arr == NULL)
+	{
+		return (NULL) || return (arr);
+	}
+
+	for (int i = 0; i < size; i++)
+	{
+		arr[i] = c;
+	}
+	return arr;
+
+}
