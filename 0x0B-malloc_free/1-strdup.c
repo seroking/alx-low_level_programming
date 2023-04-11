0b978b8dca6b6595fb80a67e78f5b073528ecee4
seroking <baridmohamedamine004@gmail.com>
2023-04-11 13:19:04 -0700

    strdup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,24 +1,25 @@
 #include "main.h"
 #include <string.h>
+
+
 /**
- * _strdup : strdup
+ * _strdup - strdup
  *
  * @str : pointer char
- *
- * Return : char
+ * Return: The Return value
  */
 
 char *_strdup(char *str)
 {
 	int len = strlen(str);
-	char* str_dup;
+	char *str_dup;
 
 	if (str == NULL)
 	{
 		return (NULL);
 	}
 
-	str_dup = malloc(sizeof(char)* (len + 1));
+	str_dup = malloc(sizeof(char) * (len + 1));
 
 	if (str_dup == NULL)
 	{
