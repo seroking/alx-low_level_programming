0e613a25839df4893508890963b470c3451eb565
seroking <baridmohamedamine004@gmail.com>
2023-04-17 13:22:05 -0700

    dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -19,5 +19,5 @@ void print_dog(struct dog *d)
 	}
 	printf("Name: %s\n", d->name ? d->name : "(nil)");
 	printf("Age: %f\n", d->age);
-	printf("Breed: %s\n", d->breed ? d->breed : "(nil)");
+	printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
 }
