6c3416f769072b37c59e25af37bdad330119caa9
seroking <baridmohamedamine004@gmail.com>
2023-04-17 08:43:11 -0700

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * struct dog - check the code
+ * @name : name
+ * @age : age
+ * @owner : owner
+ * Return: Always 0.
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
