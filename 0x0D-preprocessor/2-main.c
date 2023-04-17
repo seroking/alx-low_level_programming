b2e89507b34df773dd9f8d1dc8c10f3e8042bd6c
seroking <baridmohamedamine004@gmail.com>
2023-04-17 15:28:50 -0700

    macro

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
 int main(void)
 {
 	printf("%s\n", __FILE__);
-	return 0;
+	return (0);
 }
