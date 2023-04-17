7ec53f259a58e5c1c1ca6f61c13ec16a7c04737a
seroking <baridmohamedamine004@gmail.com>
2023-04-17 15:04:45 -0700

    dog

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - check the code
+ * @d : struct
+ * Return: Always 0.
+ */
+
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
