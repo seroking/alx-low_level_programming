ad66407e80cb9aef8fc95f187d119bb91cf2cc82
seroking <baridmohamedamine004@gmail.com>
2023-04-17 12:45:12 -0700

    dog

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		if (d->name == NULL)
+		{
+			d->name = "(nil)";
+		}
+		if (d->owner == NULL)
+		{
+			d->owner = "(nil)";
+		}
+		printf("Name : %s\nAge : %.2f\nOwner : %s\n",d->name,d->age,d->owner);
+	}
+}
