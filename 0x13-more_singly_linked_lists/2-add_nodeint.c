16d4d832807f7d33b57b585be551d3ebca3a1297
seroking <baridmohamedamine004@gmail.com>
2023-04-30 03:31:03 -0700

    tasks

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -13,14 +13,17 @@ listint_t *add_nodeint(listint_t **head, const int n)
 
 	new_node = (listint_t *) malloc(sizeof(listint_t));
 
-	new_node->n = n;
-	new_node->next = (*head);
-	(*head) = new_node;
-
 	if (!new_node)
 	{
 		return (NULL);
 	}
+
+
+	new_node->n = n;
+	new_node->next = (*head);
+	(*head) = new_node;
+
+
 
 	return (*head);
 }
