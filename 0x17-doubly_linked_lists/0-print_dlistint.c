d4f92d1c4df243b76ff57d8f9715bddd8eeaa4c1
seroking <baridmohamedamine004@gmail.com>
2023-06-15 09:25:22 -0700

    tasks

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - print all the elements of a list
+ * @h : pointer to the list
+ * Return: count
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	while (h)
+	{
+		printf("%d\n",h->n);
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
