5fc3d0935951e3cefa287538ad3417332e4211c2
seroking <baridmohamedamine004@gmail.com>
2023-06-15 09:33:10 -0700

    tasks

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,10 +1,17 @@
 #include "lists.h"
 
+
+/**
+ * dlistint_len - len of a double linked list
+ * @h : pointer to list
+ * Return: len
+ */
+
 size_t dlistint_len(const dlistint_t *h)
 {
 	size_t len = 0;
 
-	while(h)
+	while (h)
 	{
 		h = h->next;
 		len++;
