4a076a1368042010d3f5f170028e8b5895472fef
seroking <baridmohamedamine004@gmail.com>
2023-06-15 16:03:30 -0700

    tasks

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -2,24 +2,21 @@
 
 /**
  * free_dlistint - frees a dlistint_t list
- *
  * @head: head of the list
  * Return: no return
  */
+
 void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *val;
 
 	if (head != NULL)
-	{
-		while (head != NULL)
+		while (head->prev != NULL)
 			head = head->prev;
-	}
 
 	while ((val = head) != NULL)
 	{
 		head = head->next;
 		free(val);
 	}
-
 }
