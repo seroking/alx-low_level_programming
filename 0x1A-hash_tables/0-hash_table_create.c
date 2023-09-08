92baed32462e86095047902afe241dfc0001f6cb
seroking <baridmohamedamine004@gmail.com>
2023-09-08 08:07:28 -0700

    hash

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,20 +13,18 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *table;
 	unsigned long int i;
 
-	table = (hash_table_t *) malloc(sizeof(hash_table_t));
+	table = malloc(sizeof(hash_table_t));
 	if (!table)
 		return (NULL);
 
-	table->array = (hash_node_t **) malloc(sizeof(hash_node_t *) * size);
+	table->array = malloc(sizeof(hash_node_t *) * size);
 	if (!table->array)
 		return (NULL);
 
 	table->size = size;
 
 	for (i = 0; i < size; i++)
-	{
 		table->array[i] = NULL;
-	}
 
 	return (table);
 }
